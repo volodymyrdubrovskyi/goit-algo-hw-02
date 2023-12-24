3062a3adbaf05f14caa78273e78ee6683d6a2c6a
volodymyrdubrovskyi <vladimir.dubrovskiy@gmail.com>
2023-12-25 00:19:19 +0200

    v.25.12.2023_00.19

--- task1.py
+++ task1.py
@@ -1,36 +1,33 @@
 from queue import Queue
 
-
 max_while_limit = 1000
 
-# Створюємо чергу
-q = Queue()
-
-def generate_request():
-    global q
+def generate_request(q:Queue):
     req = 'Go!'
     while req:
         req = input('створити нову заявку (пустий текст для завершення вводу заявок):')
         if req != '':
             q.put(req)
             print('черга: ', q.queue)
+    return q
 
-def process_request():
-    global q
+def process_request(q:Queue):
     if q.empty():
         print('черга пуста')
     w = 0
-    while (not q.empty()) or (w < max_while_limit):
+    while (not q.empty()) and (w < max_while_limit):
         print(q.queue)
         print(f'Request {q.get()} processed....')
         print(q.queue, '\n')
         w += 1
+    return q
 
+
+q = Queue()
 not_go_exit = True
-
 w = 0
-while (not_go_exit) or (w < max_while_limit):
-    generate_request()
-    process_request()
+while (not_go_exit) and (w < max_while_limit):
+    q = generate_request(q)
+    q = process_request(q)
     not_go_exit = input('>>> Enter to STOP, other text for continue >>>')
     w += 1
